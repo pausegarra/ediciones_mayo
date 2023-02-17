62884a99717a0f8d9010b6785b5ab390c9890c5e
Pau Segarra <pausegarra.b@gmail.com>
2023-02-17 08:36:34 +0100

    search

--- app/Http/Controllers/TextController.php
+++ app/Http/Controllers/TextController.php
@@ -5,82 +5,83 @@ namespace App\Http\Controllers;
 use App\Models\Text;
 use Illuminate\Http\Request;
 use App\Http\Requests\TextsRequest;
-use App\Http\Requests\UpdateTextRequest;
 use Illuminate\Support\Facades\Storage;
+use App\Http\Requests\UpdateTextRequest;
 
 class TextController extends Controller
 {
-    /**
-     * Get texts from DB
-     *
-     * @return void
-     */
-    public function get()
-    {
-        return Text::all();
+  /**
+   * Get texts from DB
+   *
+   * @return void
+   */
+  public function get(Request $req)
+  {
+    $texts = Text::where('text', 'like', "%{$req->search}%")->orWhere('title', 'like', "%{$req->search}%")->get();
+    return $texts;
+  }
+
+  /**
+   * Save text to DB
+   *
+   * @param TextsRequest $request
+   * @return void
+   */
+  public function store(TextsRequest $request)
+  {
+    $file = $request->getFile();
+    $file->storeAs('public/texts_images', $file->getClientOriginalName());
+
+    Text::create([
+      'text'     => $request->getText(),
+      'url'      => $file->getClientOriginalName(),
+      'title'    => $request->getTitle(),
+      'doctor'   => $request->getDoctor(),
+      'location' => $request->getLocation(),
+    ]);
+  }
+
+  /**
+   * Update text from DB
+   *
+   * @param UpdateTextRequest $request
+   * @param Text $text
+   * @return void
+   */
+  public function update(UpdateTextRequest $request, Text $text)
+  {
+    if ($request->getFile() != null) {
+      Storage::disk('public')->delete('texts_images/' . $text->url);
+      $file = $request->getFile();
+      $file->storeAs('public/texts_images', $file->getClientOriginalName());
+
+      $text->url = $file->getClientOriginalName();
     }
 
-    /**
-     * Save text to DB
-     *
-     * @param TextsRequest $request
-     * @return void
-     */
-    public function store(TextsRequest $request)
-    {
-        $file = $request->getFile();
-        $file->storeAs('public/texts_images',$file->getClientOriginalName());
+    if ($request->getText() != null)
+      $text->text = $request->getText();
 
-        Text::create([
-            'text'     => $request->getText(),
-            'url'      => $file->getClientOriginalName(),
-            'title'    => $request->getTitle(),
-            'doctor'   => $request->getDoctor(),
-            'location' => $request->getLocation(),
-        ]);
-    }
+    if ($request->getTitle() != null)
+      $text->title = $request->getTitle();
 
-    /**
-     * Update text from DB
-     *
-     * @param UpdateTextRequest $request
-     * @param Text $text
-     * @return void
-     */
-    public function update(UpdateTextRequest $request, Text $text)
-    {
-        if ($request->getFile() != null) {
-            Storage::disk('public')->delete('texts_images/'.$text->url);
-            $file = $request->getFile();
-            $file->storeAs('public/texts_images',$file->getClientOriginalName());
-            
-            $text->url = $file->getClientOriginalName();
-        }
+    if ($request->getDoctor() != null)
+      $text->doctor = $request->getDoctor();
 
-        if ($request->getText() != null)
-            $text->text = $request->getText();
+    if ($request->getLocation() != null)
+      $text->location = $request->getLocation();
 
-        if ($request->getTitle() != null)
-            $text->title = $request->getTitle();
+    $text->save();
+  }
 
-        if ($request->getDoctor() != null)
-            $text->doctor = $request->getDoctor();
-
-        if ($request->getLocation() != null)
-            $text->location = $request->getLocation();
-
-        $text->save();
-    }
-
-    /**
-     * Deletes a text
-     *
-     * @param Text $text
-     * @return void
-     */
-    public function delete(Text $text) 
-    {
-        Storage::disk('public')->delete('texts_images/'.$text->url);
-        $text->delete();
-    }
+  /**
+   * Deletes a text
+   *
+   * @param Text $text
+   * @return void
+   */
+  public function delete(Text $text)
+  {
+    Storage::disk('public')->delete('texts_images/' . $text->url);
+    $text->delete();
+  }
 }
